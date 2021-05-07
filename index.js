a0e2eb67f7e4bb87fb7f19d5ca3f5e0c6a4d1c21
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-05-06 21:28:46 -0500

    Mvp?

--- index.js
+++ index.js
@@ -10,10 +10,27 @@ const PORT = process.env.PORT || 5000;
 
 server.use(express.json());
 
-server.get("/api/web", (req, res) => {
-  res.json({
-    cohort: "Web 41",
-  });
+server.get("/api/dogs", (req, res) => {
+  res.send(`<h2>LOOK! THERE ARE DOGS!</h2>
+  <p>Bobbert
+   <br><br>
+  Captain
+  <br><br>
+  Doog
+  <br><br>
+  Loog
+  <br><br>
+  Goof
+  <br><br>
+  Foog <p>
+  `);
+});
+
+server.use("*", (req, res) => {
+  res.send(`
+    <h2>Lambda Project</h2>
+    <p>Welcome!</p>
+  `);
 });
 
 server.listen(PORT, () => {
